d6b8b7630b539e9a42f1c0e5894f8df12686288f
Gustavo Sant'Anna <g_santanna@outlook.com>
2022-03-01 17:08:11 -0300

    Cria componente para renderizar tags de redes socias

---
+++ src/components/SocialIcon.tsx
@@ -0,0 +1,17 @@
+import { IconType } from "react-icons";
+
+interface SocialIconProps { Component: IconType, iconName: string, bgColor: string };
+
+const SocialIcon = ({ Component, iconName, bgColor }: SocialIconProps) => {
+  return (
+    <div
+      className={`grid grid-cols-[25%_75%] items-center gap-1
+      rounded shadow-lg ${bgColor} text-white px-2 py-0.5 text-lg`}
+    >
+      <Component className={`${iconName}-tag text-2xl text-white`} />
+      { iconName }
+    </div>
+  );
+}
+
+export default SocialIcon;
