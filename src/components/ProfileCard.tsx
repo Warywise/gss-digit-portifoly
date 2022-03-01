78bfac0d0b36fe2c2ab2735747c2baf10ce89f4c
Gustavo Sant'Anna <g_santanna@outlook.com>
2022-03-01 17:09:00 -0300

    Implementa e estiliza tags de redes sociais no "ProfileCard"

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,4 +1,7 @@
 import { NextComponentType } from 'next';
+import { FaGithubSquare, FaLinkedin } from 'react-icons/fa';
+import { BiMailSend } from 'react-icons/bi';
+import SocialIcon from './SocialIcon';
 
 const ProfileCard: NextComponentType = () => {
   return (
@@ -11,9 +14,21 @@ const ProfileCard: NextComponentType = () => {
         <h4>Desenvolvedor Web FullStack Jr</h4>
       </div>
       <div className="profile-card-social">
-        <a href="https://github.com/Warywise" target="_blank">GitHub</a>
-        <a href="https://www.linkedin.com/in/g-s-s/" target="_blank">LinkedIn</a>
-        <a href="mailto:g_santanna@outlook.com?subject=PortifolyFeedback">Envie um Email</a>
+        <a href="https://github.com/Warywise" target="_blank">
+          <SocialIcon
+            Component={FaGithubSquare} iconName={'GitHub'} bgColor="bg-orange-600"
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/g-s-s/" target="_blank">
+          <SocialIcon
+            Component={FaLinkedin} iconName={'LinkedIn'} bgColor="bg-blue-600"
+          />
+        </a>
+        <a href="mailto:g_santanna@outlook.com?subject=PortifolyFeedback">
+        <SocialIcon
+            Component={BiMailSend} iconName={'Email'} bgColor="bg-emerald-600"
+          />
+        </a>
       </div>
     </div>
   );
