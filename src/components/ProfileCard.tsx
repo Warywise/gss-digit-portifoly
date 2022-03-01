bbba4683e7b4764f5d7a9d1d474a17fd886aa3d7
Gustavo Sant'Anna <g_santanna@outlook.com>
2022-03-01 04:22:25 -0300

    Estilizações básicas de componente "ProfileCard" concluídas

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -4,17 +4,17 @@ const ProfileCard: NextComponentType = () => {
   return (
     <div className="profile-card">
       <span className="profile-image">
-        <img
-          src="/gust-sant.jpeg"
-          className="image"
-          alt="Gustavo Sant'Anna"
-          />
-        </span>
-      <h2>Gustavo Sant'Anna</h2>
-      <h4>Desenvolvedor Web FullStack Jr</h4>
-      <a href="https://github.com/Warywise" target="_blank">GitHub</a>
-      <a href="https://www.linkedin.com/in/g-s-s/" target="_blank">LinkedIn</a>
-      <a href="mailto:g_santanna@outlook.com?subject=PortifolyFeedback">Envie um Email</a>
+        <img src="/gust-sant.jpeg" className="image" alt="Gustavo Sant'Anna" />
+      </span>
+      <div className="profile-card-content">
+        <h2>Gustavo Sant'Anna</h2>
+        <h4>Desenvolvedor Web FullStack Jr</h4>
+      </div>
+      <div className="profile-card-social">
+        <a href="https://github.com/Warywise" target="_blank">GitHub</a>
+        <a href="https://www.linkedin.com/in/g-s-s/" target="_blank">LinkedIn</a>
+        <a href="mailto:g_santanna@outlook.com?subject=PortifolyFeedback">Envie um Email</a>
+      </div>
     </div>
   );
 }
