a5bf6271aae05ff45ac8d869c5f364a66a121992
Gustavo Sant'Anna <g_santanna@outlook.com>
2022-03-06 14:01:00 -0300

    Cria arquivo de "mock" para dados de projetos

---
+++ src/PROJECTS_DATA.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    name: 'Nyan Catrometer',
+    img: 'https://user-images.githubusercontent.com/85768828/156932072-b2657998-4515-4fbf-ad20-d660108808a9.gif',
+    url: 'https://warywise.github.io/nyan-cronometer/',
+    gitRep: 'https://github.com/Warywise/nyan-cronometer',
+  },
+  {
+    name: 'ChoqueFlix',
+    img: 'https://user-images.githubusercontent.com/85768828/156933469-e5670217-d20d-4838-bbdb-4b053f03a95b.gif',
+    url: 'https://choqueflix.vercel.app/',
+    gitRep: 'https://github.com/Warywise/choqueflix',
+  }
+];
