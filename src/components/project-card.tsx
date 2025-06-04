f15ff170f523c22073f6c79f88152415624b1859
Gustavo Sant'Anna <g_santanna@outlook.com>
2025-06-04 19:16:14 -0300

    style: some adjustments

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -44,10 +44,11 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
     };
   }, []);
 
+  // TODO: mudar os containers para joga bortões pra baixo e alinhar info section
   return (
     <article
       ref={elementRef}
-      className="bg-card rounded-lg overflow-hidden shadow-sm border border-border animate-fade-in post-animation"
+      className="flex flex-col justify-between rounded-lg overflow-hidden shadow-sm border border-border animate-fade-in post-animation"
     >
       {/* Project Image */}
       <div className="relative cursor-pointer" onClick={() => setIsDetailsOpen(true)}>
@@ -65,75 +66,77 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
       </div>
 
       {/* Project Info */}
-      <div className="p-4">
-        <div className="flex justify-between items-center mb-2">
-          <h3
-            className="font-medium text-lg line-clamp-1 cursor-pointer hover:text-primary transition-colors"
-            onClick={() => setIsDetailsOpen(true)}
-          >
-            {project.name}
-          </h3>
-          <span className="text-xs text-text">{project.commits} commits</span>
-        </div>
-
-        <p className="text-sm text-text/80 line-clamp-2 mb-4">{project.description}</p>
-
-        {/* Tags */}
-        <div className="flex flex-wrap gap-2 mb-4">
-          {project.techStacks.map((tech) => (
-            <Badge key={tech} label={tech} variant="outline" style="text-xs bg-secondary/50" />
-          ))}
-        </div>
-
-        {/* Interaction Buttons */}
-        <div className="flex items-center justify-between pt-2 border-t border-border">
-          <div className="flex items-center space-x-4">
-            {/* TODO: fazer variants para o tooltip */}
-            <Tooltip title={isLiked ? 'Unlike' : 'Like'}>
-              <Button
-                label={
-                  <>
-                    <FaRocket
-                      size={18}
-                      className={isLiked ? 'fill-accent text-accent' : 'text-text/80'}
-                    />
-                    <span className="ml-1 text-xs">{likeCount}</span>
-                  </>
-                }
-                variant="ghost"
-                size="sm"
-                style="py-1 h-auto text-text/80"
-                onClick={() => setIsLiked(!isLiked)}
-              />
-            </Tooltip>
-
-            <Tooltip title="Coment">
-              <Button
-                variant="ghost"
-                size="sm"
-                style="py-1 h-auto text-text/80"
-                onClick={() => {}}
-                label={
-                  <>
-                    <FaMessage size={18} />
-                    <span className="ml-1 text-xs">{project.comments}</span>
-                  </>
-                }
-              />
-            </Tooltip>
+      <div className="px-4 pt-4">
+        <div>
+          <div className="flex justify-between items-center mb-2">
+            <h3
+              className="font-medium text-lg line-clamp-1 cursor-pointer hover:text-primary transition-colors"
+              onClick={() => setIsDetailsOpen(true)}
+            >
+              {project.name}
+            </h3>
+            <span className="text-xs text-text">{project.commits} commits</span>
           </div>
 
-          <div className="flex items-center space-x-4">
-            <Tooltip title="Share">
-              <Button
-                label={<FaShare size={18} />}
-                variant="ghost"
-                size="sm"
-                style="p-1 h-auto text-text/80"
-                onClick={() => {}}
-              />
-            </Tooltip>
+          <p className="text-sm text-text/80 line-clamp-2 mb-4">{project.description}</p>
+
+          {/* Tags */}
+          <div className="flex flex-wrap gap-2 mb-4">
+            {project.techStacks.map((tech) => (
+              <Badge key={tech} label={tech} variant="outline" style="text-xs bg-secondary/50" />
+            ))}
           </div>
+        </div>
+      </div>
+
+      {/* Interaction Buttons */}
+      <div className="flex items-center justify-between pt-2 border-t border-border">
+        <div className="flex items-center space-x-4">
+          {/* TODO: fazer variants para o tooltip */}
+          <Tooltip title={isLiked ? 'Unlike' : 'Like'}>
+            <Button
+              label={
+                <>
+                  <FaRocket
+                    size={18}
+                    className={isLiked ? 'fill-accent text-accent' : 'text-text/80'}
+                  />
+                  <span className="ml-1 text-xs">{likeCount}</span>
+                </>
+              }
+              variant="link"
+              size="sm"
+              style="pj-button"
+              onClick={() => setIsLiked(!isLiked)}
+            />
+          </Tooltip>
+
+          <Tooltip title="Coment">
+            <Button
+              variant="link"
+              size="sm"
+              style="pj-button"
+              onClick={() => {}}
+              label={
+                <>
+                  <FaMessage size={18} />
+                  <span className="ml-1 text-xs">{project.comments}</span>
+                </>
+              }
+            />
+          </Tooltip>
+        </div>
+
+        <div className="flex items-center px-4 pb-4">
+          <Tooltip title="Share">
+            <Button
+              label={<FaShare size={18} />}
+              variant="link"
+              size="sm"
+              style="pj-button"
+              onClick={() => {}}
+            />
+          </Tooltip>
         </div>
       </div>
     </article>
