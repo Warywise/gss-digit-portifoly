59f6d42b4df33d716523faa5923cc38ea6dbbd40
Gustavo Sant'Anna <g_santanna@outlook.com>
2025-06-11 02:05:56 -0300

    feat: search input to filter projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,47 @@
 'use client';
+import { useState } from 'react';
 import Profile from '@/components/profile';
 import ProjectCard from '@/components/project-card';
 import projectsData from '@/PROJECTS_DATA';
+import { FaSearch } from 'react-icons/fa';
+import { FaRegCircleQuestion } from 'react-icons/fa6';
 
 export default function Home() {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const filteredProjects = projectsData.filter(
+    (project) =>
+      project.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      project.description.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      project.techStacks.some((tech) => tech.toLowerCase().includes(searchTerm.toLowerCase())),
+  );
+
   return (
-    <div className="grid grid-rows-[1fr_5fr] items-center justify-items-center min-h-screen p-8 gap-16 sm:p-16 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid items-start justify-items-center min-h-screen p-8 gap-16 sm:p-16">
       <Profile />
       <main className="flex flex-col gap-16 row-start-2 items-center sm:items-start w-full">
-        <div className="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-6 w-full">
-          {projectsData.map((project) => (
+        <div className="mb-6 flex flex-col md:flex-row justify-between items-center gap-4 w-full">
+          <h2 className="text-2xl font-bold">Projects</h2>
+
+          <div className="relative w-full md:w-64">
+            <FaSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-placeholder h-3 w-3 md:h-4 md:w-4" />
+            <input
+              placeholder="Search projects..."
+              className={`search-input ${searchTerm ? 'w-full!' : ''}`}
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-6 w-10/12 md:w-full">
+          {(searchTerm ? filteredProjects : projectsData).map((project) => (
             <ProjectCard key={project.id} project={project} />
           ))}
+          {searchTerm && !filteredProjects.length && (
+            <h4 className="text-lg flex gap-4 items-center">
+              <FaRegCircleQuestion /> No projects found
+            </h4>
+          )}
         </div>
       </main>
     </div>
--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -44,53 +44,54 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
     };
   }, []);
 
-  // TODO: mudar os containers para joga bortões pra baixo e alinhar info section
   return (
     <article
       ref={elementRef}
       className="flex flex-col justify-between rounded-lg overflow-hidden shadow-sm border border-border animate-fade-in post-animation"
     >
-      {/* Project Image */}
-      <div className="relative cursor-pointer" onClick={() => setIsDetailsOpen(true)}>
-        <Image
-          loading="lazy"
-          src={project.img}
-          alt={project.name}
-          className="w-full aspect-video object-cover"
-          width={width}
-          height={width}
-        />
-        <div className="absolute top-4 right-4">
-          {project.deployed && <Badge label="Deployed" style="bg-primary text-foreground" />}
-        </div>
-      </div>
-
-      {/* Project Info */}
-      <div className="px-4 pt-4">
-        <div>
-          <div className="flex justify-between items-center mb-2">
-            <h3
-              className="font-medium text-lg line-clamp-1 cursor-pointer hover:text-primary transition-colors"
-              onClick={() => setIsDetailsOpen(true)}
-            >
-              {project.name}
-            </h3>
-            <span className="text-xs text-text">{project.commits} commits</span>
+      <div>
+        {/* Project Image */}
+        <div className="relative cursor-pointer" onClick={() => setIsDetailsOpen(true)}>
+          <Image
+            loading="lazy"
+            src={project.img}
+            alt={project.name}
+            className="w-full aspect-video object-cover"
+            width={width}
+            height={width}
+          />
+          <div className="absolute top-4 right-4">
+            {project.deployed && <Badge label="Deployed" style="bg-primary text-foreground" />}
           </div>
+        </div>
 
-          <p className="text-sm text-text/80 line-clamp-2 mb-4">{project.description}</p>
+        {/* Project Info */}
+        <div className="px-4 pt-4">
+          <div>
+            <div className="flex justify-between items-center mb-2">
+              <h3
+                className="font-medium text-lg line-clamp-1 cursor-pointer hover:text-primary transition-colors"
+                onClick={() => setIsDetailsOpen(true)}
+              >
+                {project.name}
+              </h3>
+              <span className="text-xs text-text">{project.commits} commits</span>
+            </div>
 
-          {/* Tags */}
-          <div className="flex flex-wrap gap-2 mb-4">
-            {project.techStacks.map((tech) => (
-              <Badge key={tech} label={tech} variant="outline" style="text-xs bg-secondary/50" />
-            ))}
+            <p className="text-sm text-text/80 line-clamp-2 mb-4">{project.description}</p>
+
+            {/* Tags */}
+            <div className="flex flex-wrap gap-2 mb-4">
+              {project.techStacks.map((tech) => (
+                <Badge key={tech} label={tech} variant="outline" style="text-xs bg-secondary/50" />
+              ))}
+            </div>
           </div>
         </div>
       </div>
 
       {/* Interaction Buttons */}
-      <div className="flex items-center justify-between pt-2 border-t border-border">
+      <div className="flex items-center justify-between pt-2 pl-2 border-t border-border">
         <div className="flex items-center space-x-4">
           {/* TODO: fazer variants para o tooltip */}
           <Tooltip title={isLiked ? 'Unlike' : 'Like'}>
@@ -106,7 +107,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
               }
               variant="link"
               size="sm"
-              style="pj-button"
+              style="proj-button"
               onClick={() => setIsLiked(!isLiked)}
             />
           </Tooltip>
@@ -115,7 +116,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
             <Button
               variant="link"
               size="sm"
-              style="pj-button"
+              style="proj-button"
               onClick={() => {}}
               label={
                 <>
@@ -133,7 +134,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
               label={<FaShare size={18} />}
               variant="link"
               size="sm"
-              style="pj-button"
+              style="proj-button"
               onClick={() => {}}
             />
           </Tooltip>
