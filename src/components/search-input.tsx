ee32476279686b2b0ce4dddd4a505644d8885980
Gustavo Sant'Anna <g_santanna@outlook.com>
2026-08-22 00:41:15 -0300

    refact: simplify filter and search functionality in Projects components

--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -1,30 +1,15 @@
 'use client';
-import React, { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import React, { Dispatch, SetStateAction } from 'react';
 import { FaSearch } from 'react-icons/fa';
-import ProjectsModel from '@/types/projects';
 import { useTranslations } from 'next-intl';
 
 interface SearcInputProps {
-  handleSearch: Dispatch<SetStateAction<ProjectsModel[]>>;
-  projectsData: ProjectsModel[];
+  searchTerm: string;
+  setSearchTerm: Dispatch<SetStateAction<string>>;
 }
 
-const SearchInput: React.FC<SearcInputProps> = ({ handleSearch, projectsData }) => {
+const SearchInput: React.FC<SearcInputProps> = ({ searchTerm, setSearchTerm }) => {
   const t = useTranslations('Projects');
-  const [searchTerm, setSearchTerm] = useState<string | null>(null);
-
-  useEffect(() => {
-    if (searchTerm || searchTerm !== null) {
-      const filteredProjects = projectsData.filter(
-        (project) =>
-          project.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-          project.description.toLowerCase().includes(searchTerm.toLowerCase()) ||
-          project.techStacks.some((tech) => tech.toLowerCase().includes(searchTerm.toLowerCase())),
-      );
-
-      handleSearch(filteredProjects);
-    }
-  }, [searchTerm, handleSearch, projectsData]);
 
   return (
     <div className="relative w-[75%] md:w-64 place-self-end row-start-1 col-start-3 mr-1">
@@ -32,7 +17,7 @@ const SearchInput: React.FC<SearcInputProps> = ({ handleSearch, projectsData }) 
       <input
         placeholder={t('searchPlaceholder')}
         className={`search-input ${searchTerm ? 'w-full!' : ''}`}
-        value={searchTerm || ''}
+        value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
     </div>
